0c337d4788f4441f5c3d57c02ec87c2254e49f33
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-03-22 16:03:54 +0300

    кнопочки

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./Components/Header/Header";
+import Button from "./Components/Button/Button";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./Components/ProductList/ProductList";
+import {FOCUSABLE_SELECTOR} from "@testing-library/user-event/dist/utils";
+import Form from "./Components/Form/Form";
+
 
 
 function App() {
@@ -14,7 +20,10 @@ function App() {
     return (
         <div className="App">
             <Header></Header>
-            <button onClick={onToggleButton}>Нажать на кнопку</button>
+            <Routes>
+                <Route index element={<ProductList/>}></Route>
+                <Route path={'form'} element={<Form/>}></Route>
+            </Routes>
         </div>
     )
 }
